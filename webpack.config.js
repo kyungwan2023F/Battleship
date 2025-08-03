17ec663c3f279a99901d3ba619806c7e075a871e
John Doe <149332198+kyungwan2023F@users.noreply.github.com>
2025-08-03 08:27:04 -0400

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+// webpack.config.mjs
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+export default {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve("dist"),
+    clean: true,
+  },
+  mode: "development",
+  devServer: {
+    static: "./dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
